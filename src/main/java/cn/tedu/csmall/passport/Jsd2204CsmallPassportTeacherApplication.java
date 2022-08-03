2b701816bd06db1864df11ada3b729b7e34c79dd
liucangsong <liucangsong@gmail.com>
2022-08-03 17:28:25 +0800

    .

---
+++ src/main/java/cn/tedu/csmall/passport/Jsd2204CsmallPassportTeacherApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsd2204CsmallPassportTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsd2204CsmallPassportTeacherApplication.class, args);
+    }
+
+}
